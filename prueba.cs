ed94527836ae47be918208b1ad5fb27919032069
Jeanpierre Ortiz <JEANPIERREORTIZA10@GMAIL.COM>
2026-03-04 08:37:42 -0500

    Se creo una suma archivo .cs

---
+++ prueba.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("Hola mundo");
+Console.WriteLine("aca haremos una pequeña suma");
+int a = console.readline();
+int b = console.readline();
+int suma = a + b;
+Console.WriteLine("La suma de a y b es: " + suma);
